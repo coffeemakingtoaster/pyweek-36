cab841372cff590d92ce02d0742ba1a4dbcdc6ce
Ben Häußermann <ben.haussermann@gmail.com>
2023-09-18 15:57:01 +0200

    boundingbox

--- entities/room.py
+++ entities/room.py
@@ -2,6 +2,8 @@ from direct.showbase import DirectObject
 from config import MAP_CONSTANTS
 import json
 from helpers.model_helpers import load_model
+from panda3d.core import BoundingBox
+from panda3d.core import LPoint3
 
 class Room(DirectObject.DirectObject):
       
@@ -12,6 +14,7 @@ class Room(DirectObject.DirectObject):
         self.id = id
         self.gridPos = gridPos
         self.roomAssets = self.loadRoomAssets(id)
+        self.boundingBox = None
         
         self.models = []
         
@@ -26,8 +29,8 @@ class Room(DirectObject.DirectObject):
             model=load_model(asset["asset"])
             model.reparentTo(render)
             model.setPos(asset["x"]+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE,asset["y"],asset["z"]+self.gridPos[1]*MAP_CONSTANTS.ROOM_SIZE)
-    
             self.models.append(model)
+        self.boundingBox = BoundingBox(LPoint3(-MAP_CONSTANTS.ROOM_SIZE/2+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE,-MAP_CONSTANTS.ROOM_SIZE/2+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE,-MAP_CONSTANTS.ROOM_SIZE/2+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE),LPoint3(MAP_CONSTANTS.ROOM_SIZE/2+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE,MAP_CONSTANTS.ROOM_SIZE/2+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE,MAP_CONSTANTS.ROOM_SIZE/2+self.gridPos[0]*MAP_CONSTANTS.ROOM_SIZE))
         return self
             
     def destroy(self):
